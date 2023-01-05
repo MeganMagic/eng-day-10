f88db8fa8cef5fda25d6a57261b506a01fc24288
JUNGHO CHOI <75521675+9rganizedchaos@users.noreply.github.com>
2023-01-05 23:19:28 +0900

    feat: CommitteeSection 구현 (#6)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import OutlineSection from "../sections/OutlineSection";
 import TalkSection from "../sections/TalkSection";
 import NetworkingSection from "../sections/NetworkingSection";
 import TimetableSection from "../sections/TimetableSection";
+import CommitteeSection from "../sections/CommitteeSection";
 
 const MainPageWrapper = styled.div`
   background-color: #000000;
@@ -20,6 +21,7 @@ const MainPage: React.FC = () => {
       <TalkSection />
       <NetworkingSection />
       <TimetableSection />
+      <CommitteeSection />
     </MainPageWrapper>
   );
 };
