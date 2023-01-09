7b27049c862358298337c0f89cd2308c92f22abe
MeganMagic <at.fence.3@gmail.com>
2023-01-09 18:05:53 +0900

    feat: animation 구현 화면 merge

--- src/components/Interaction/Interaction.tsx
+++ src/components/Interaction/Interaction.tsx
@@ -9,8 +9,8 @@ import {
   TABLET_VALUE,
   DESKTOP_VALUE,
   WIDE_VALUE,
-  P5Color,
   Device,
+  colors,
 } from "./constants";
 
 const Interaction: React.FC = () => {
@@ -26,12 +26,15 @@ const Interaction: React.FC = () => {
   let padding: number;
   let pivotGap: number;
   let edgeLength: number;
+  let velocity: number;
 
-  let dots = [];
-  let edges = [];
+  let dots: Dot[] = [];
+  let edges_mobile: Edge[][] = [];
+  let edges_tablet: Edge[][] = [];
+  let edges_desktop: Edge[][] = [];
 
-  let scene: number = 300;
-  let sceneNumber: number = 3;
+  let scene: number = 200;
+  let sceneNumber: number = 4;
   let totalScene: number = scene * sceneNumber;
   let frame = (frameCount) => {
     return frameCount % totalScene;
@@ -79,10 +82,18 @@ const Interaction: React.FC = () => {
         new Vector(0, 0),
         new Vector(boxWidth, boxHeight)
       );
-      const weight = p5.map(dist, 0, maxDist / 2, radius / 5, 0);
-      const opacityWeight = p5.map(dist, 0, maxDist / 2, 0.75, 0);
-      const color = `rgba(${KEY_COLOR.rgb.r}, ${KEY_COLOR.rgb.g}, ${KEY_COLOR.rgb.b}, ${opacityWeight})`;
-      this.display(p5, this.pos.x, this.pos.y, radius + weight, color);
+
+      const radiusWeight = Math.trunc(p5.map(dist, 0, maxDist / 2, 10, 0));
+      const opacityWeight = p5.map(dist, 0, maxDist / 2, 0.5, 0);
+      const { r, g, b } = KEY_COLOR.rgb;
+
+      this.display(
+        p5,
+        this.pos.x,
+        this.pos.y,
+        radius + radiusWeight,
+        `rgba(${r}, ${g}, ${b}, ${opacityWeight})`
+      );
     }
   }
 
@@ -92,19 +103,23 @@ const Interaction: React.FC = () => {
     targetPos;
     velocity;
     isMoved;
+    tx;
+    ty;
     xs;
     ys;
 
     constructor(x: number, y: number, tx: number, ty: number) {
       super(x, y);
       this.mPos = new Vector(this.pos.x, this.pos.y);
+      this.tx = tx;
+      this.ty = ty;
       this.targetPos = new Vector(
         padding + radius + tx * pivotGap,
         padding + radius + ty * pivotGap
       );
       this.velocity = window.p5.Vector.sub(this.targetPos, this.pos)
         .normalize()
-        .setMag(8);
+        .setMag(velocity);
       this.isMoved = false;
       this.xs = [];
       this.ys = [];
@@ -115,11 +130,31 @@ const Interaction: React.FC = () => {
       this.ys.push(this.mPos.y);
     }
 
+    resetPos() {
+      this.pos = new Vector(
+        padding + radius + this.x * pivotGap,
+        padding + radius + this.y * pivotGap
+      );
+      this.mPos = new Vector(this.pos.x, this.pos.y);
+      this.targetPos = new Vector(
+        padding + radius + this.tx * pivotGap,
+        padding + radius + this.ty * pivotGap
+      );
+      this.velocity = window.p5.Vector.sub(this.targetPos, this.pos)
+        .normalize()
+        .setMag(velocity);
+      this.xs = [];
+      this.ys = [];
+    }
+
     move(p5: p5Types, startFrame) {
       if (frame(p5.frameCount) === 0) {
-        // setting init
         this.isMoved = false;
         this.mPos = new Vector(this.pos.x, this.pos.y);
+      }
+
+      if (frame(p5.frameCount) <= startFrame) {
+        return;
       }
 
       if (this.isMoved === false) {
@@ -130,12 +165,21 @@ const Interaction: React.FC = () => {
       }
 
       for (let i = 0; i < this.xs.length; i += 1) {
-        const mappedColor = `rgba(
-          ${KEY_COLOR.rgb.r},
-          ${KEY_COLOR.rgb.g},
-          ${KEY_COLOR.rgb.b},
-          ${p5.map(i, 0, this.xs.length, 0, 1)}
-        )`;
+        const expandIndex = Math.floor(
+          p5.map(
+            i,
+            this.xs.length - edgeLength,
+            this.xs.length,
+            colors.length - 1,
+            0
+          )
+        );
+        const collapseIndex = Math.floor(
+          p5.map(i, 0, edgeLength, colors.length - 1, 0)
+        );
+        const mappedColor = !this.isMoved
+          ? colors[expandIndex]
+          : colors[collapseIndex];
 
         this.display(p5, this.xs[i], this.ys[i], radius, mappedColor);
       }
@@ -147,26 +191,7 @@ const Interaction: React.FC = () => {
     }
   }
 
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
-    init();
-    p5.createCanvas(boxWidth, boxHeight).parent(canvasParentRef);
-
-    const edge1 = new Edge(1, 1, 2, 2);
-    edges.push(edge1);
-  };
-
-  const draw = (p5: p5Types) => {
-    p5.background(BG_COLOR.hex);
-
-    for (let edge of edges) edge.move(p5, 120);
-    for (let dot of dots) {
-      dot.pulse(p5);
-      dot.display(p5);
-    }
-    p5.text(`(${p5.mouseX}, ${p5.mouseY})`, p5.mouseX, p5.mouseY);
-  };
-
-  const init = () => {
+  const checkDevice = () => {
     const parent = document.querySelector('div[data-testid="react-p5"]');
     const { clientWidth } = parent;
 
@@ -174,6 +199,141 @@ const Interaction: React.FC = () => {
     else if (clientWidth >= 1280) device = "DESKTOP";
     else if (clientWidth >= 768) device = "TABLET";
     else device = "MOBILE";
+  };
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    init();
+    p5.createCanvas(boxWidth, boxHeight).parent(canvasParentRef);
+
+    // animation 정의
+    edges_tablet = [
+      [
+        new Edge(1, 1, 2, 0),
+        new Edge(0, 0, 1, 1),
+        new Edge(2, 1, 1, 2),
+        new Edge(3, 1, 2, 2),
+        new Edge(0, 2, 1, 3),
+      ],
+      [
+        new Edge(0, 1, 1, 0),
+        new Edge(2, 0, 3, 1),
+        new Edge(2, 2, 1, 3),
+        new Edge(1, 1, 2, 2),
+        new Edge(3, 2, 2, 3),
+      ],
+      [
+        new Edge(1, 2, 0, 3),
+        new Edge(2, 2, 3, 3),
+        new Edge(0, 1, 1, 2),
+        new Edge(3, 1, 2, 2),
+        new Edge(1, 0, 0, 1),
+        new Edge(2, 0, 3, 1),
+      ],
+      [
+        new Edge(2, 1, 3, 0),
+        new Edge(1, 0, 2, 1),
+        new Edge(0, 1, 1, 0),
+        new Edge(1, 3, 0, 2),
+        new Edge(3, 2, 2, 3),
+      ],
+    ];
+    edges_desktop = [
+      [
+        new Edge(1, 1, 2, 0),
+        new Edge(0, 0, 1, 1),
+        new Edge(2, 1, 1, 2),
+        new Edge(3, 1, 2, 2),
+      ],
+      [
+        new Edge(0, 1, 1, 0),
+        new Edge(2, 1, 3, 0),
+        new Edge(2, 2, 1, 1),
+        new Edge(3, 1, 2, 2),
+      ],
+      [
+        new Edge(1, 1, 0, 0),
+        new Edge(2, 1, 3, 0),
+        new Edge(0, 2, 1, 1),
+        new Edge(3, 2, 2, 1),
+      ],
+      [
+        new Edge(0, 1, 1, 2),
+        new Edge(1, 0, 0, 1),
+        new Edge(2, 0, 3, 1),
+        new Edge(3, 1, 2, 2),
+      ],
+    ];
+    edges_mobile = [
+      [
+        new Edge(1, 1, 2, 0),
+        new Edge(0, 0, 1, 1),
+        new Edge(1, 2, 2, 3),
+        new Edge(2, 3, 1, 4),
+        new Edge(1, 3, 0, 4),
+      ],
+      [
+        new Edge(1, 0, 0, 1),
+        new Edge(2, 0, 1, 1),
+        new Edge(1, 3, 0, 4),
+        new Edge(0, 2, 1, 3),
+        new Edge(1, 2, 2, 3),
+      ],
+      [
+        new Edge(1, 0, 2, 1),
+        new Edge(0, 1, 1, 0),
+        new Edge(0, 3, 1, 4),
+        new Edge(1, 2, 0, 3),
+        new Edge(1, 4, 2, 3),
+      ],
+      [
+        new Edge(1, 0, 0, 1),
+        new Edge(2, 1, 1, 0),
+        new Edge(0, 2, 1, 3),
+        new Edge(1, 4, 2, 3),
+        new Edge(0, 3, 1, 4),
+      ],
+    ];
+  };
+
+  const draw = (p5: p5Types) => {
+    p5.background(BG_COLOR.hex);
+
+    checkDevice();
+    switch (device) {
+      case "WIDE":
+        for (let i = 0; i < edges_desktop.length; i++) {
+          let session = edges_desktop[i];
+          for (let edge of session) edge.move(p5, scene * i);
+        }
+        break;
+      case "DESKTOP":
+        for (let i = 0; i < edges_desktop.length; i++) {
+          let session = edges_desktop[i];
+          for (let edge of session) edge.move(p5, scene * i);
+        }
+        break;
+      case "TABLET":
+        for (let i = 0; i < edges_tablet.length; i++) {
+          let session = edges_tablet[i];
+          for (let edge of session) edge.move(p5, scene * i);
+        }
+        break;
+      case "MOBILE":
+      default:
+        for (let i = 0; i < edges_mobile.length; i++) {
+          let session = edges_mobile[i];
+          for (let edge of session) edge.move(p5, scene * i);
+        }
+    }
+
+    for (let dot of dots) {
+      dot.pulse(p5);
+      dot.display(p5);
+    }
+  };
+
+  const init = () => {
+    checkDevice();
 
     switch (device) {
       case "WIDE":
@@ -185,6 +345,7 @@ const Interaction: React.FC = () => {
         padding = WIDE_VALUE.padding;
         pivotGap = WIDE_VALUE.pivotGap;
         edgeLength = WIDE_VALUE.edgeLength;
+        velocity = WIDE_VALUE.velocity;
         break;
       case "DESKTOP":
         boxWidth = DESKTOP_VALUE.boxWidth;
@@ -195,6 +356,7 @@ const Interaction: React.FC = () => {
         padding = DESKTOP_VALUE.padding;
         pivotGap = DESKTOP_VALUE.pivotGap;
         edgeLength = DESKTOP_VALUE.edgeLength;
+        velocity = DESKTOP_VALUE.velocity;
         break;
       case "TABLET":
         boxWidth = TABLET_VALUE.boxWidth;
@@ -205,6 +367,7 @@ const Interaction: React.FC = () => {
         padding = TABLET_VALUE.padding;
         pivotGap = TABLET_VALUE.pivotGap;
         edgeLength = TABLET_VALUE.edgeLength;
+        velocity = TABLET_VALUE.velocity;
         break;
       default:
         boxWidth = MOBILE_VALUE.boxWidth;
@@ -215,6 +378,7 @@ const Interaction: React.FC = () => {
         padding = MOBILE_VALUE.padding;
         pivotGap = MOBILE_VALUE.pivotGap;
         edgeLength = MOBILE_VALUE.edgeLength;
+        velocity = MOBILE_VALUE.velocity;
     }
 
     let newDots = [];
@@ -229,6 +393,10 @@ const Interaction: React.FC = () => {
   const windowResized: SketchProps["windowResized"] = (p5) => {
     init();
     p5.resizeCanvas(boxWidth, boxHeight);
+
+    for (let scene of edges_desktop) for (let edge of scene) edge.resetPos();
+    for (let scene of edges_tablet) for (let edge of scene) edge.resetPos();
+    for (let scene of edges_mobile) for (let edge of scene) edge.resetPos();
   };
 
   return (
